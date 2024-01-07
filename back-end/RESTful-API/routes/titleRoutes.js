530161e4652e82fa55da1e12d029d2891e579df4
ntua-el20167 <147545582+ntua-el20167@users.noreply.github.com>
2024-01-07 17:02:33 +0200

    Add files via upload

--- back-end/RESTful-API/routes/titleRoutes.js
+++ back-end/RESTful-API/routes/titleRoutes.js
@@ -7,7 +7,7 @@ router.get('/title/:titleID', validateToken, titleController.getTitleByID);
 router.get('/searchtitle', validateToken, titleController.searchTitleByPart);
 router.get('/bygenre/', validateToken, titleController.getTitlesByGenre);
 router.get('/titles/:titleID/details', validateToken, titleController.getTitleDetails);
-router.get('/home', validateToken, titleController.getHomepageData);
+router.get('/home',validateToken, titleController.getHomepageData);
 router.get('/tv-shows/episodes',validateToken, titleController.getAllTvShowsEpisodes);
 
 module.exports = router;
