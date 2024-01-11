436c40399b0e69d684cb2ccd2f4e08d76e65f57f
ntua-el20158 <72971725+ntua-el20158@users.noreply.github.com>
2024-01-11 12:56:37 +0200

    Add files via upload

--- back-end/RESTful-API/app.js
+++ back-end/RESTful-API/app.js
@@ -3,10 +3,18 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const nodemon = require('nodemon');
 const mysql2 = require('mysql2');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+
+const openapiSpecification = YAML.load('./openapi.yaml');
+
 
 const app = express();
 
 app.use(cors());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
